7d53f29934a89a43cd0c04738f5e7868fd9ff718
eminemtnn27 <eminemetin9@gmail.com>
2021-05-30 12:43:59 +0300

    Js ödev-2 yapıldı

---
+++ javascript/ödev-2/js/toDo.js
@@ -0,0 +1,10 @@
+var list=document.getElementsByTagName("li");
+var close = document.getElementsByClassName("close");
+
+
+// load items
+loadItems();
+
+// call event listeners
+eventListeners();
+
