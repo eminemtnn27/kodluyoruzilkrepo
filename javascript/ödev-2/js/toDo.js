1335b329103c3fca71c49e7e5437e1134bb6d337
eminemtnn27 <eminemetin9@gmail.com>
2021-05-30 13:37:22 +0300

    Js ödev-2 yapıldı

--- javascript/ödev-2/js/toDo.js
+++ javascript/ödev-2/js/toDo.js
@@ -1,10 +1,34 @@
-var list=document.getElementsByTagName("li");
-var close = document.getElementsByClassName("close");
+var myList=document.getElementsByTagName("LI");
+var close = document.getElementsByClassName("close"); 
+var list=document.getElementById('list');
 
+function newElement(){
+    var span=document.createElement("span");
+    var kapat = document.createTextNode("x");
+    var inputTxt=document.getElementById('task').value;
+    var li=document.createElement("li");
+    var t=document.createTextNode(inputTxt);
+    li.appendChild(t);
+    if (inputTxt === "") {
+        $(".error").toast("show");
+      } else {
+        $(".success").toast("show");
+        document.getElementById("list").appendChild(li);
+      }
+      document.getElementById("task").value = "";
+    
+    span.className="close";
+    span.appendChild(kapat);
+    li.appendChild(span)
 
-// load items
-loadItems();
-
-// call event listeners
-eventListeners();
-
+    for(var i=0; i<close.length;i++)
+    {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+            };
+    }
+   
+    }
+ 
+ 
