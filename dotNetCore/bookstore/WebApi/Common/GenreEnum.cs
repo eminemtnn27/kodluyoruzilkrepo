539a7bc69df2b415af6b140878bb5f9ce91640e1
eminemtnn27 <eminemetin9@gmail.com>
2022-02-10 17:40:26 +0300

    projeler oluşturuldu

---
+++ dotNetCore/bookstore/WebApi/Common/GenreEnum.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Common
+{
+    public enum GenreEnum
+    {
+         PersonalGrowth=1,
+         ScienceFiction,
+         Noval,  
+    }
+}
---
+++ dotNetCore/bookstore/WebApi/Controllers/BookController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.DeleteBook;
+using WebApi.BookOperations.GetBookDetail;
+using WebApi.BookOperations.GetBooks;
+using WebApi.BookOperations.UpdateBook;
+using WebApi.DbOperations;
+using static WebApi.BookOperations.UpdateBook.UpdateBookCommand;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController:ControllerBase
+    {
+        private readonly IBookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public BookController(IBookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+     
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context,_mapper);
+            var result=query.Handle();
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            BookDetailViewModel result;
+            //try
+            //{
+                GetBookDetailQuery query = new GetBookDetailQuery(_context,_mapper);
+                query.BookId = id;
+                result=query.Handle();
+            //}
+            //catch(Exception ex)
+            //{
+            //    return BadRequest(ex.Message);
+            //}
+            return Ok(result);
+        }
+        //[HttpGet]
+        //public Book Get([FromQuery] string id)
+        //{
+        //    var book=BookList.Where(book=>book.Id==Convert.ToInt32(id)).SingleOrDefault();
+        //    return book; 
+        //}
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+            CreateBookCommand command = new CreateBookCommand(_context,_mapper); 
+            command.Model = newBook;
+            CreateBookCommandValidator validator = new CreateBookCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle(); 
+            return Ok(); 
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id,[FromBody] UpdateBookModel updatedBook)
+        { 
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            command.bookId = id;
+            command.Model = updatedBook;
+            command.Handle(); 
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        { 
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            command.bookId = id;
+            DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle(); 
+            return Ok();
+        }
+    }
+}
