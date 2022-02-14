a0b007ee70c7dd0b076e140227e47dbb1ba8191a
eminemtnn27 <eminemetin9@gmail.com>
2022-02-14 13:19:30 +0300

    proje oluşturuldu

---
+++ c#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı Girin");
+            string name= Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girin");
+            string surname= Console.ReadLine();
+            Console.WriteLine("merhaba " +name+" "+surname);
+
+        }
+    }
+}
