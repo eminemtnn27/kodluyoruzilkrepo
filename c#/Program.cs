ab27789150fe4d248e83d4a2a459d54567103b30
eminemtnn27 <eminemetin9@gmail.com>
2022-02-28 11:23:37 +0300

    alıştırma yapıldı

--- c#/Program.cs
+++ c#/Program.cs
@@ -6,7 +6,7 @@ namespace c_
     {
         static void Main(string[] args)
         {
-            // try
+             // try
             //    {
             //        //int a = int.Parse(null);
             //        //int a = int.Parse("test");
@@ -117,7 +117,6 @@ namespace c_
             // Console.Write("Lütfen dizinin eleman sayısını giriniz: ");
             // int diziUzunlugu=int.Parse(Console.ReadLine());
             // int[] sayiDizisi=new int[diziUzunlugu];
-
             // for(int i=0;i<diziUzunlugu;i++)
             // {
             //     Console.Write("Lütfen {0}.sayıyı giriniz: ",i+1);
@@ -130,7 +129,239 @@ namespace c_
 
             // Console.WriteLine("Ortalama : " +toplam/diziUzunlugu);
 
-            Console.WriteLine();
+            //sort (sıralama işlemi yapar)
+
+            // int[] sayiDizisi={23,11,6,86,49,7,22,32};
+            // Console.WriteLine("*** Sırasız Dizi ***");
+            // foreach(var sayi in sayiDizisi)
+            //     Console.WriteLine(sayi);
+
+            // Console.WriteLine("*** Sıralı Dizi ***");
+            // Array.Sort(sayiDizisi);
+
+            // foreach(var sayi in sayiDizisi)
+            //     Console.WriteLine(sayi);
+
+            // //clear (Dizinin belirtilen elemanlarını varsayılan değerine getirir)
+            // Console.WriteLine("*** Array Clear ***");
+
+            // //sayı dizisi elemanlarını kullanarak 2.index ten itibaren 2 tane elemanı sıfırlar
+            // Array.Clear(sayiDizisi,2,2);
+
+            // foreach(var sayi in sayiDizisi)
+            //     Console.WriteLine(sayi);
+
+            // //reverse(dizinin ilk elemanı ile son elemanını yer değiştirir)
+            // Console.WriteLine("*** Array Reverse ***");
+            // Array.Reverse(sayiDizisi);
+
+            // foreach(var sayi in sayiDizisi)
+            //     Console.WriteLine(sayi);
+
+            // //indexof
+            // Console.WriteLine("*** Array indexOf  ***");
+            // Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            // //resize(Dizileri yeniden boyutlandırır)
+            // Console.WriteLine("*** Array resize  ***");
+            // Array.Resize<int>(ref sayiDizisi,9);
+            // sayiDizisi[8]=99;
+
+            // foreach(var sayi in sayiDizisi)
+            //     Console.WriteLine(sayi);
+
+
+            //erişim_belirteci    geri_dönüş_tipi    metot_adı(parametre listesi / argüman)
+            //{
+                //komutlar;
+                //return;
+            //}
+
+            //   int a = 2;
+            //  int b = 3;
+            //   Console.WriteLine(a + b);
+
+            //   int sonuc = Topla(a, b);
+            //    Console.WriteLine(sonuc);
+
+            //    Metotlar ornek = new Metotlar();
+            //    ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            //    int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+            //     ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            //     ornek.EkranaYazdir(Convert.ToString(a + b));
+            //    }
+
+            //   static int Topla(int deger1, int deger2)
+            //   {
+            //      return (deger1 + deger2);
+            //   }
+            // }
+
+            // class Metotlar
+            // {
+            //     public void EkranaYazdir(string veri)
+            //     {
+            //         Console.WriteLine(veri);
+            //     }
+
+            //     public int ArttirVeTopla(ref int deger1, ref int deger2)
+            //     {
+            //         deger1 += 1;
+            //         deger2 += 1;
+            //         return deger1 + deger2;     
+
+            //     }
+            //}
+
+            //Out Parametreler
+            //         string sayi = "999";
+
+            //         bool sonuc = int.TryParse(sayi, out int outSayi);
+            //         if (sonuc)
+            //         {
+            //             Console.WriteLine("Başarılı!");
+            //             Console.WriteLine(outSayi);
+            //         }
+            //         else
+            //         {
+            //             Console.WriteLine("Başarısız!");
+            //         }
+
+            //         Metotlar instance = new Metotlar();
+            //         instance.Topla(4, 5, out int toplamSonucu);
+            //         Console.WriteLine(toplamSonucu);
+
+            //         //Metot Aşırı Yükleme - Overloading
+            //         int ifade = 999;
+            //         instance.EkranaYazdir(Convert.ToString(ifade));
+            //         instance.EkranaYazdir(ifade);
+            //         instance.EkranaYazdir("Emine", "Metin");
+
+            //         //Metot İmzası
+            //         //metot adı + parametre sayısı + parametre
+            //     }
+            // }
+
+            //     class Metotlar
+            //     {
+            //         public void Topla(int a, int b, out int toplam)
+            //         {
+            //             toplam = a + b;
+            //         }
+
+            //         public void EkranaYazdir(string veri)
+            //         {
+            //             Console.WriteLine(veri);
+            //         }
+
+            //         public void EkranaYazdir(int veri)
+            //         {
+            //             Console.WriteLine(veri);
+            //         }
+            //         public void EkranaYazdir(string veri1, string veri2)
+            //         {
+            //             Console.WriteLine(veri1 + veri2);
+            //         }
+            //     } 
+
+           
+            // Rekürsif(Yinelemeli) Fonksiyonlar :Kendi kendini çağırabilirler.
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= 3;
+            }
+            Console.WriteLine(result);
+
+            Islemler islem1 = new();
+            Console.WriteLine(islem1.Expo(3, 4));
+            /*
+            Extension Metotların kelime anlamı genişletilebilir metotlardır. C# 3.0 ile hayatımıza giren, aynı kodu defalarca yazmak yerine, bir kere yazıp her yerden erişebileceğimiz kolay kullanımı olan metotlardır.
+            */
+            string ifade = "Berkcan Gümüşışık";
+            bool sonuc = ifade.CheckSpaces();
+            Console.WriteLine(sonuc);
+            if (sonuc)
+            {
+                Console.WriteLine(ifade.RemoveWhiteBackSpaces());
+            }
+
+            Console.WriteLine(ifade.MakeUpperCase());
+            Console.WriteLine(ifade.MakeLowerCase());
+
+            int[] dizi = { 9, 3, 6, 2, 5, 0 };
+            dizi.SortArray();
+            dizi.EkranaYazdir();
+
+            int sayi = 5;
+            Console.WriteLine(sayi.IsEventNumber());
+            Console.WriteLine(ifade.GetFirstCharacter());
         }
     }
+
+    public class Islemler
+    {
+        public int Expo(int sayi, int us)
+        {
+            if (us < 2)
+            {
+                return sayi;
+            }
+            return Expo(sayi, us - 1) * sayi;
+        }
+    }
+
+    public static class Extension
+    {
+        public static bool CheckSpaces(this string param)
+        {
+            return param.Contains(" ");
+        }
+
+        public static string RemoveWhiteBackSpaces(this string param)
+        {
+            string[] dizi = param.Split(" ");
+            return string.Join("-", dizi);
+        }
+
+        public static string MakeUpperCase(this string param)
+        {
+            return param.ToUpper();
+        }
+
+        public static string MakeLowerCase(this string param)
+        {
+            return param.ToLower();
+        }
+
+        public static int[] SortArray(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+
+        public static void EkranaYazdir(this int[] param)
+        {
+            foreach (var item in param)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static bool IsEventNumber(this int value)
+        {
+            if (value % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static string GetFirstCharacter(this string param)
+        {
+            return param.Substring(0,1);
+        }
+    }
+
 }
+
