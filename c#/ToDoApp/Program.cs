06a8872a157e85703133f330753f84c439a08f1e
eminemtnn27 <eminemetin9@gmail.com>
2022-02-28 14:03:11 +0300

    alıştırma yapıldı

---
+++ c#/ToDoApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        }
+         public static int Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçin");
+            Console.WriteLine("************************************");
+            Console.WriteLine("(1) Kart Eklemek");
+            Console.WriteLine("(2) Kart Guncelle");
+            Console.WriteLine("(3) Kart Sil");
+            Console.WriteLine("(4) Kart Taşı");
+            Console.WriteLine("(5) Board Listelemek");
+            Console.WriteLine("(6) Çıkış");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
