9319aedb7c5690f78fe90d513355539495ff1145
Emine Metin <eminemetin9@gmail.com>
2023-06-12 21:28:18 +0300

    java101 work created

--- java/Java101/src/ucakBileti.java
+++ java/Java101/src/ucakBileti.java
@@ -2,62 +2,57 @@ import java.util.Scanner;
 
 public class ucakBileti {
     public static void main(String[] args) {
-        int km, yas, yolTip;
-        Scanner veri = new Scanner(System.in);
-        Double ucrKm = 0.10, cocuk = 0.5, genc = 0.90, ihtiyar = 0.70, gidDon = 0.80;
+
+        int km, age, road;
+        Scanner data = new Scanner(System.in);
+        Double perKm = 0.10, child = 0.5, young = 0.90, old = 0.70, roundTrip = 0.80;
+
+
         System.out.print("Mesafeyi km türünden giriniz : ");
-        km = veri.nextInt();
+        km = data.nextInt();
         System.out.print("Yaşınızı giriniz : ");
-        yas = veri.nextInt();
-        System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ):");
-        yolTip = veri.nextInt();
+        age = data.nextInt();
+        System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
+        road = data.nextInt();
 
-        if (yas < 12) {
-            if (yolTip == 1) {
-                System.out.println("Normal tutar : " + (km*ucrKm) + " TL");
-                System.out.println("İndirimli tutar : " + (km * ucrKm * cocuk) + " TL");
-            } else if (yolTip == 2) {
-                System.out.println("Normal tutar : " + (2 * (km * ucrKm)) + " TL");
-                System.out.println("İndirimli tutar : " + (2 * (km * ucrKm * cocuk * gidDon)) + " TL");
+        if (age < 12) {
+            if (road == 1) {
+                System.out.println("Toplam tutar : " + (km * perKm * child) + " TL");
+            } else if (road == 2) {
+                System.out.println("Toplam tutar :" + (2 * (km * perKm * child * roundTrip)) + " TL");
             } else {
-                System.out.println("Hatalı giriş yaptınız.");
+                System.out.println("Hatalı Veri Girdiniz !");
             }
 
         }
-        if (yas > 12 && yas < 24) {
-            if (yolTip == 1) {
-                System.out.println("Normal tutar : " + (km*ucrKm) + " TL");
-                System.out.println("İndirimli tutar : " + (km * ucrKm * genc) + " TL");
-            } else if (yolTip == 2) {
-                System.out.println("Normal tutar : " + (2 * (km * ucrKm)) + " TL");
-                System.out.println("İndirimli tutar : " + (2 * (km * ucrKm * genc * gidDon)) + " TL");
+        if (age > 12 && age < 24) {
+            if (road == 1) {
+                System.out.println("Toplam tutar : " + (km * perKm * young) + " TL");
+            } else if (road == 2) {
+                System.out.println("Toplam tutar : " + (2 * (km * perKm * young * roundTrip)) + " TL");
             } else {
-                System.out.println("Hatalı giriş yaptınız.");
+                System.out.println("Hatalı Veri Girdiniz !");
             }
 
         }
-        if (yas > 65) {
-            if (yolTip == 1) {
-                System.out.println("Normal tutar : " + (km*ucrKm) + " TL");
-                System.out.println("İndirimli tutar : " + (km * ucrKm * ihtiyar) + " TL");
-            } else if (yolTip == 2) {
-                System.out.println("Normal tutar : " + (2 * (km * ucrKm)) + "TL");
-                System.out.println("İndirimli tutar : " + (2 * (km * ucrKm * ihtiyar * gidDon)) + " TL");
+        if (age > 65) {
+            if (road == 1) {
+                System.out.println("Toplam tutar : " + (km * perKm * old) + " TL");
+            } else if (road == 2) {
+                System.out.println("Toplam tutar : : " + (2 * (km * perKm * old * roundTrip)) + " TL");
             } else {
-                System.out.println("Hatalı giriş yaptınız.");
+                System.out.println("Hatalı Veri Girdiniz !");
             }
 
         }
-        if (yas <= 65 && yas >= 24) {
-            if (yolTip == 1) {
-                System.out.println("Bilet tutarı : " + (km*ucrKm) + " TL");
-            } else if (yolTip == 2) {
-                System.out.println("Normal tutar : " + (2 * (km * ucrKm)) + "TL");
-                System.out.println("İndirimli tutar : " + (2 * (km * ucrKm *  gidDon)) + " TL");
+        if (age <= 65 && age >= 24) {
+            if (road == 1) {
+                System.out.println("Toplam tutar : " + (km*perKm) + " TL");
+            } else if (road == 2) {
+                System.out.println("Toplam tutar : " + (2 * (km * perKm *  roundTrip)) + " TL");
             } else {
-                System.out.println("Hatalı giriş yaptınız.");
+                System.out.println("Hatalı Veri Girdiniz !");
             }
-
         }
 
     }
